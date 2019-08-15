4909df7d46cf528ba3f32bdfa439edf7a088ac8a
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-15 12:22:20 +0100

    edited the ubuntu version

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,7 +10,7 @@ describe 'node::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'converges successfully' do
       expect{ chef_run }.to_not raise_error
